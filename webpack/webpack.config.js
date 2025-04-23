1e88f5122fe8b0e047bc203be2caa4761ea33255
Denis Garpinich <d.garpinich@wisetech.pro>
2025-04-23 12:59:08 +0300

    Сделана настройка webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,12 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {merge} = require('webpack-merge');
+const commonConfig = require('./webpack.common.config.js');
+const productionConfig = require('./webpack.production.config.js');
+const developmentConfig = require('./webpack.development.config.js');
 
-module.exports = {
-  module : {
-    rules :
-    [{test : /\.css$/, use : [MiniCssExtractPlugin.loader, 'css-loader']}]
-  },
-  plugins :
-          [
-            new HtmlWebpackPlugin({template : './src/index.html'}),
-            new MiniCssExtractPlugin()
-          ],
-  devServer : {static : './dist', port : 8081, watchFiles : ['src/*']},
+module.exports = function(env) {
+  if (env.production) {
+    return merge(commonConfig, productionConfig);
+  } else {
+    return merge(commonConfig, developmentConfig);
+  }
 }
